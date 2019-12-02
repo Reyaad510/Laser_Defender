babd0cd91925a43000b64c4639284e38cdfb6603
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-02 14:01:28 -0500

    Player can move left and right

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Movement done here because want to check movement every frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    private void Move() {
+        // can use var to know it will be a float
+        var deltaX = Input.GetAxis("Horizontal");
+        var newXPos = transform.position.x + deltaX;
+        transform.position = new Vector2(newXPos, transform.position.y);
+       
+
+    }
+
+
+}
