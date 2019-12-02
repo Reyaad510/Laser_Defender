399f7ba8f8446b6c8e81129483d9a01e33482f5c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-02 14:09:27 -0500

    Added player movement for vertical movement. Incorporated Time.deltaTime

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +23,21 @@ public class Player : MonoBehaviour
 
     private void Move() {
         // can use var to know it will be a float
-        var deltaX = Input.GetAxis("Horizontal");
+        // Look below for Time.deltaTime explanation
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPos = transform.position.x + deltaX;
-        transform.position = new Vector2(newXPos, transform.position.y);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
        
 
     }
 
 
 }
+
+
+//Time.deltaTime
+// Game will behave same on fast and slow computers
+// Says "how long did it take for that frame take to execute"
+ // something multiplied by Time.deltaTime will make it frame independent
