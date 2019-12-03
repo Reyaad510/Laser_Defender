485a4d32a9635d332bf5c188ca325c6150d22c99
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-02 21:57:32 -0500

    Created Scriptable Object called WaveConfig

---
+++ Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This puts EnemyWaveConfig when you go to "Create Empty" in Unity
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+
+//Scriptable bcuz holding values for other scripts to grab
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+
+    public GameObject PathPrefab()
+    {
+        return pathPrefab;
+    }
+
+    public float TimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+
+    public float SpawnRandomFactor()
+    {
+        return spawnRandomFactor;
+    }
+
+    public int NumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+
+    public float MoveSpeed()
+    {
+        return moveSpeed;
+    }
+    
+
+}
