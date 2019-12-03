0a35b791d41575afdfb50f84065a73462309df5f
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-03 18:21:35 -0500

    Created Scene for Main Menu with UI and Scene for Game over with UI

---
+++ Laser Defender/Assets/Scripts/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+
+    
+public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+public void LoadGame()
+    {
+        SceneManager.LoadScene("Game"); // Grabbing Scene named called "Game". Not best way if u change name later on
+    }
+
+public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
