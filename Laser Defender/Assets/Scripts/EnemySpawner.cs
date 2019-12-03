1134e3f6566082fb07645f1499102af8d99c1a03
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-03 00:11:08 -0500

    Able to spawn multiple waves at once

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -6,13 +6,24 @@ public class EnemySpawner : MonoBehaviour
 {
 
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+        }
     }
 
 
@@ -22,7 +33,7 @@ public class EnemySpawner : MonoBehaviour
         for (int enemyCount = 0; enemyCount < waveConfig.NumberOfEnemies(); enemyCount++)
         {
 
-                
+             // creating new enemy and then passing in that specific enenmy wave information(allowing each wave to have diff speeds etc)
             var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
             newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.TimeBetweenSpawns());
